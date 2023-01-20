49492ffe0d570b5b882161f73ea526ce6696ff8c
呱呱 <804626212@qq.com>
2023-01-20 14:42:40 +0800

    添加MY_usart.c/MY_usart.h

---
+++ ROS_Robot_FW/Core/BSP/MY_usart.c
@@ -0,0 +1,5 @@
+//
+// Created by WYH on 2023/1/20.
+//
+
+#include "MY_usart.h"
---
+++ ROS_Robot_FW/Core/BSP/MY_usart.h
@@ -0,0 +1,8 @@
+//
+// Created by WYH on 2023/1/20.
+//
+
+#ifndef ROS_ROBOT_MY_USART_H
+#define ROS_ROBOT_MY_USART_H
+
+#endif //ROS_ROBOT_MY_USART_H
